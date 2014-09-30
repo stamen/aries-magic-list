377c1e1490fc9da915df1389750e34eb1bd8cf64
Brandon Liu <bdon@bdon.org>
2014-09-30 16:26:05 -0700

    JSON conversion script also corrects names

--- convert.py
+++ convert.py
@@ -2,8 +2,11 @@ import csv
 data = csv.DictReader(open('World-z10.tsv'), delimiter="\t")
 import fiona
 import json
+import leveldb
 
 d = []
+
+db = leveldb.LevelDB("db/geonames")
 
 for row in data:
   latitude = float(row['latitude'])
@@ -13,7 +16,17 @@ for row in data:
       'type': 'Point',
       'coordinates': [longitude,latitude]
   }
+
+  geonameid = str(row['geonameid'])
   name = unicode(row['name'],'utf-8')
+
+  try:
+    correct_name = unicode(db.Get(geonameid))
+    if name != correct_name:
+      name = correct_name
+  except Exception:
+    pass
+
   f['properties'] = {'name':name,'population':int(row['population']),'zoom':int(row['zoom start']),'capital':row['capital']}
   f['type'] = 'Feature'
   d.append(f)
